20b9c6fe4c730631be9085a97bb7083c91792cde
Charanjeet Singh Sidhu <62802350+cssidhu0001@users.noreply.github.com>
2023-01-04 11:09:37 +0530

    Calculator work Done

--- Level1/CalculatorLevel1Task3/calci.js
+++ Level1/CalculatorLevel1Task3/calci.js
@@ -1,64 +1,27 @@
- function calculate(){
-    
+
+function valueButton(valueFromUser){
+
+    calculatorform.outputInput.value +=valueFromUser.value;
 }
 function ac(){
-    document.getElementById("caclioperation").innerHTML = " ";
+    calculatorform.outputInput.value =null;
 }
 
-function del(){
-    document.getElementById("caclioperation").innerHTML =document.getElementById("caclioperation").innerHTML -"0";
+function calculate(){
+    calculatorform.outputInput.value=eval(document.getElementById("caclioperation").value)
 }
-function bracket1(){
-    const brac1=document.getElementById("caclioperation").innerHTML =document.getElementById("caclioperation").innerHTML + "(";
+function sqrt(){
+    calculatorform.outputInput.value=Math.sqrt(document.getElementById("caclioperation").value)
 }
-function bracket2(){
-    const brac2=document.getElementById("caclioperation").innerHTML  =document.getElementById("caclioperation").innerHTML +  ")";
+function square(){
+    let squareNumber=document.getElementById("caclioperation").value;
+    calculatorform.outputInput.value=squareNumber*squareNumber;
+}
+function cube(){
+    let cubeNumber=document.getElementById("caclioperation").value;
+    calculatorform.outputInput.value=cubeNumber*cubeNumber*cubeNumber;
 }
 function mod(){
-    const modu=document.getElementById("operatorUsed").innerHTML  =document.getElementById("caclioperation").innerHTML + "%";
+
 }
-function num9(){
-   const numb9= document.getElementById("caclioperation").innerHTML  =document.getElementById("caclioperation").innerHTML + "9";
-}
-function num8(){
-    const numb8=document.getElementById("caclioperation").innerHTML  =document.getElementById("caclioperation").innerHTML +  "8";
-}
-function num7(){
-    const numb7=document.getElementById("caclioperation").innerHTML =document.getElementById("caclioperation").innerHTML + "7";
-}
-function num6(){
-    const numb6= document.getElementById("caclioperation").innerHTML  =document.getElementById("caclioperation").innerHTML +  "6";
-}
-function num5(){
-    const numb5=document.getElementById("caclioperation").innerHTML =document.getElementById("caclioperation").innerHTML +  "5";
-}
-function num4(){
-    const numb4=document.getElementById("caclioperation").innerHTML  =document.getElementById("caclioperation").innerHTML +  "4";
-}
-function num3(){
-    const numb3=document.getElementById("caclioperation").innerHTML  =document.getElementById("caclioperation").innerHTML +  "3";
-}
-function num2(){
-    const numb2=document.getElementById("caclioperation").innerHTML  =document.getElementById("caclioperation").innerHTML + "2";
-}
-function num1(){
-    const numb1=document.getElementById("caclioperation").innerHTML  =document.getElementById("caclioperation").innerHTML + "1";
-}
-function num0(){
-    const numb0=document.getElementById("caclioperation").innerHTML  =document.getElementById("caclioperation").innerHTML +  "0";
-}
-function num00(){
-    const numb00=document.getElementById("caclioperation").innerHTML  =document.getElementById("caclioperation").innerHTML +  "00";
-}
-function add(){
-    const addition=document.getElementById("operatorUsed").innerHTML = "+";
-}
-function sub(){
-    const subtract=document.getElementById("operatorUsed").innerHTML = "-";
-}
-function divide(){
-    const division=document.getElementById("operatorUsed").innerHTML = "/";
-}
-function mul(){
-    const multiply=document.getElementById("operatorUsed").innerHTML = "*";
-}
+
